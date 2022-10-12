f73b6af7ff160e71720cf54a2fc00dcec4ba0b6e
Patrick Iacob <patrick.hv.2002@gmail.com>
2022-10-11 17:24:28 -0700

    Initial commit

---
+++ lib/map.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_map/flutter_map.dart'; // Suitable for most situations
+import 'package:flutter_map/plugin_api.dart'; // Only import if required functionality is not exposed by default
+import 'package:latlong2/latlong.dart';
+
+class map extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return FlutterMap(
+      options: MapOptions(
+        center: LatLng(51.5, -0.09),
+        zoom: 5,
+      ),
+      nonRotatedChildren: [
+        AttributionWidget.defaultWidget(
+          source: 'OpenStreetMap contributors',
+          onSourceTapped: () {},
+        ),
+      ],
+      children: [
+        TileLayer(
+          urlTemplate:
+          'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+          userAgentPackageName: 'dev.fleaflet.flutter_map.example',
+        ),
+      ],
+    );
+  }
+}
