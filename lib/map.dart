8806b6142c338624ab037da970bdab958c1aed11
Patrick Iacob <patrick.hv.2002@gmail.com>
2022-10-18 17:33:31 -0700

    Added home page that then opens map

--- lib/map.dart
+++ lib/map.dart
@@ -23,6 +23,51 @@ class map extends StatelessWidget{
           'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
           userAgentPackageName: 'dev.fleaflet.flutter_map.example',
         ),
+        PolylineLayer(
+          polylineCulling: false,
+          polylines: [
+            Polyline(
+              points: [LatLng(30, 40), LatLng(20, 50), LatLng(25, 45),],
+              color: Colors.blue,
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+}
+
+class lined_map extends StatelessWidget {
+  const lined_map({Key? key,required this.cords}) : super(key: key);
+  final List<LatLng> cords;
+  @override
+  Widget build(BuildContext context) {
+    return FlutterMap(
+      options: MapOptions(
+        center: LatLng(51.5, -0.09),
+        zoom: 5,
+      ),
+      nonRotatedChildren: [
+        AttributionWidget.defaultWidget(
+          source: 'OpenStreetMap contributors',
+          onSourceTapped: () {},
+        ),
+      ],
+      children: [
+        TileLayer(
+          urlTemplate:
+          'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+          userAgentPackageName: 'dev.fleaflet.flutter_map.example',
+        ),
+        PolylineLayer(
+          polylineCulling: false,
+          polylines: [
+            Polyline(
+              points: cords,
+              color: Colors.blue,
+            ),
+          ],
+        ),
       ],
     );
   }
