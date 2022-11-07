2a546fd71ed5afba4eadbdbd9a6af8fe9f66ba02
StanleyHale <82476210+StanleyCHale@users.noreply.github.com>
2022-11-06 17:20:44 -0800

    Resolved Exception in the Console output of the PPHikeWidget: 'incorrect use of parentdatawidget'

--- lib/ProfilePage/Custom_Widgets/PPHikeWidget.dart
+++ lib/ProfilePage/Custom_Widgets/PPHikeWidget.dart
@@ -21,150 +21,146 @@ class PPHikeWidget extends StatelessWidget {
         maxHeight: 120,
       ),
 
-      child: Expanded(
-        child: Container(
-          width: double.infinity,
-          //height: double.infinity,
-          decoration: BoxDecoration(
+      child: Container(
+        width: double.infinity,
+        //height: double.infinity,
+        decoration: BoxDecoration(
 
-            borderRadius: const BorderRadius.all(Radius.circular(3.0)),
-            border: Border.all(
-              color: const Color.fromARGB(255,55, 89, 65),
-              width: 6.0,
-            ),
+          borderRadius: const BorderRadius.all(Radius.circular(3.0)),
+          border: Border.all(
+            color: const Color.fromARGB(255,55, 89, 65),
+            width: 6.0,
           ),
+        ),
 
-          child: Expanded(
-            child: Row(
+        child: Row(
 
-              children: <Widget>[
-                Container(
-                  margin: const EdgeInsets.fromLTRB(0, 0, 20, 15),
-                  child: IconButton(
-                    //Brings you to the planner focusing on the hike that was selected.
-                    onPressed: () {
-                      print('Taking to panner page with hike title: ${hikedata.title}');
-                    },
-                    icon: const Icon(
-                      Icons.location_on_sharp,
-                      size: 55,
+          children: <Widget>[
+            Container(
+              margin: const EdgeInsets.fromLTRB(0, 0, 20, 15),
+              child: IconButton(
+                //Brings you to the planner focusing on the hike that was selected.
+                onPressed: () {
+                  print('Taking to panner page with hike title: ${hikedata.title}');
+                },
+                icon: const Icon(
+                  Icons.location_on_sharp,
+                  size: 55,
+                  color: Color.fromARGB(255,55, 89, 65),
+                ),
+              ),
+            ),
+
+            Expanded(
+              child: Stack(
+                children:[
+
+                  Align(
+                    alignment: const Alignment(-1.0, -0.3),
+                    child: ConstrainedBox(
+                      constraints: const BoxConstraints(
+                        minWidth: 100,
+                        minHeight: 60,
+                        maxWidth: 190,
+                        maxHeight: 60,
+                      ),
+                      child: Text(
+                        hikedata.title,
+                        textAlign: TextAlign.left,
+                        maxLines: 2,
+                        style: const TextStyle(
+                          //fontWeight: FontWeight.bold,
+                          fontSize: 18,
+                          color: Color.fromARGB(255,55, 89, 65),
+                        ),
+                      ),
+                    ),
+                  ),
+
+                  Align(
+                    alignment: const Alignment(-1.0, 0.6),
+                    child: RatingBar(
+                      itemSize: 25,
+                      itemPadding: const EdgeInsets.fromLTRB(0, 0, 0, 0),
+                      minRating: 1,
+                      maxRating: 5,
+                      initialRating: hikedata.difficulty,
+                      allowHalfRating: true,
+                      ratingWidget: RatingWidget(
+                        full: const Icon(
+                          Icons.star_outlined,
+                          size: 5,
+                          color: Color.fromARGB(255, 249, 246, 154),
+                        ),
+                        half: const Icon(
+                          Icons.star_half_outlined,
+                          size: 5,
+                          color: Color.fromARGB(255, 249, 246, 154),
+                        ),
+                        empty: const Icon(
+                          Icons.star_outline,
+                          size: 5,
+                          color: Color.fromARGB(255, 249, 246, 154),
+                        ),
+                      ),
+                      ignoreGestures: true,
+                      onRatingUpdate: (rating) {
+                        print(rating);
+                      },
+                    ),
+                  ),
+
+                  Align(
+                    alignment: const Alignment(0.2, 0.6),
+                    child: Text(
+                      '${hikedata.distance} mi',
+                      textAlign: TextAlign.center,
+                      maxLines: 1,
+                      style: const TextStyle(
+                        //fontWeight: FontWeight.bold,
+                        fontSize: 14,
+                        color: Color.fromARGB(255,55, 89, 65),
+                      ),
+                    ),
+                  ),
+
+                  const Align(
+                    alignment: Alignment(0.8, 0.6),
+                    child: Icon(
+                      Icons.library_books,
+                      size: 35,
                       color: Color.fromARGB(255,55, 89, 65),
                     ),
                   ),
-                ),
 
-                Expanded(
-                  child: Stack(
-                    children:[
-
-                      Align(
-                        alignment: const Alignment(-1.0, -0.3),
-                        child: ConstrainedBox(
-                          constraints: const BoxConstraints(
-                            minWidth: 100,
-                            minHeight: 60,
-                            maxWidth: 190,
-                            maxHeight: 60,
-                          ),
-                          child: Text(
-                            hikedata.title,
-                            textAlign: TextAlign.left,
-                            maxLines: 2,
-                            style: const TextStyle(
-                              //fontWeight: FontWeight.bold,
-                              fontSize: 18,
-                              color: Color.fromARGB(255,55, 89, 65),
-                            ),
-                          ),
-                        ),
-                      ),
-
-                      Align(
-                        alignment: const Alignment(-1.0, 0.6),
-                        child: RatingBar(
-                          itemSize: 25,
-                          itemPadding: const EdgeInsets.fromLTRB(0, 0, 0, 0),
-                          minRating: 1,
-                          maxRating: 5,
-                          initialRating: hikedata.difficulty,
-                          allowHalfRating: true,
-                          ratingWidget: RatingWidget(
-                            full: const Icon(
-                              Icons.star_outlined,
-                              size: 5,
-                              color: Color.fromARGB(255, 249, 246, 154),
-                            ),
-                            half: const Icon(
-                              Icons.star_half_outlined,
-                              size: 5,
-                              color: Color.fromARGB(255, 249, 246, 154),
-                            ),
-                            empty: const Icon(
-                              Icons.star_outline,
-                              size: 5,
-                              color: Color.fromARGB(255, 249, 246, 154),
-                            ),
-                          ),
-                          ignoreGestures: true,
-                          onRatingUpdate: (rating) {
-                            print(rating);
-                          },
-                        ),
-                      ),
-
-                      Align(
-                        alignment: const Alignment(0.2, 0.6),
-                        child: Text(
-                          '${hikedata.distance} mi',
-                          textAlign: TextAlign.center,
-                          maxLines: 1,
-                          style: const TextStyle(
-                            //fontWeight: FontWeight.bold,
-                            fontSize: 14,
-                            color: Color.fromARGB(255,55, 89, 65),
-                          ),
-                        ),
-                      ),
-
-                      const Align(
-                        alignment: Alignment(0.8, 0.6),
-                        child: Icon(
-                          Icons.library_books,
-                          size: 35,
-                          color: Color.fromARGB(255,55, 89, 65),
-                        ),
-                      ),
-
-                      const Align(
-                        alignment: Alignment(0.65, -0.76),
-                        child: Icon(
-                          Icons.check_box_outlined,
-                          size: 35,
-                          color: Color.fromARGB(255,55, 89, 65),
-                        ),
-                      ),
-
-                      const Align(
-                        alignment: Alignment(0.95, -0.8),
-                        child: Icon(
-                          Icons.lock_outline_rounded,
-                          size: 35,
-                          color: Color.fromARGB(255,55, 89, 65),
-                        ),
-                      ),
-
-                    ],
-
+                  const Align(
+                    alignment: Alignment(0.65, -0.76),
+                    child: Icon(
+                      Icons.check_box_outlined,
+                      size: 35,
+                      color: Color.fromARGB(255,55, 89, 65),
+                    ),
                   ),
-                ),
 
-              ],
+                  const Align(
+                    alignment: Alignment(0.95, -0.8),
+                    child: Icon(
+                      Icons.lock_outline_rounded,
+                      size: 35,
+                      color: Color.fromARGB(255,55, 89, 65),
+                    ),
+                  ),
 
+                ],
+
+              ),
             ),
-          ),
 
+          ],
 
         ),
+
+
       ),
     );
   }
